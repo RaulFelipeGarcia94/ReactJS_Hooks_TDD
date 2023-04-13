0729d0cfcc269ea311364940390bdeee116ad317
Raul Garcia <raulfgarcia10@gmail.com>
2023-04-12 22:05:32 -0300

    chore: resolve dependency

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,9 @@
 module.exports = {
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
-  collectCoverageFrom: [
-    "<rootDir>/src/**/*.{ts,tsx}",
-    "!<rootDir>/src/main/**/*",
-    "!<rootDir>/src/**/index.ts",
-    "!**/*.d.ts",
-  ],
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
   coverageDirectory: "coverage",
-  testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/tests/e2e/cypress",
-  ],
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: "node",
   transform: {
     ".+\\.(ts|tsx)$": "ts-jest",
-  },
-  moduleNameMapper: {
-    "@/tests/(.*)": "<rootDir>/tests/$1",
-    "@/(.*)": "<rootDir>/src/$1",
-    "\\.scss$": "identity-obj-proxy",
   },
 };
