a51040a731ceafeadd4b42f42938e6501991c13d
Raul Garcia <raulfgarcia10@gmail.com>
2023-05-13 11:30:44 -0300

    refactor: move SUT creation to a factory method

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -2,7 +2,11 @@ import { AxiosHttpClient } from "./axios-http-client";
 import axios from "axios";
 
 jest.mock("axios"); // para não ter que fazer chamadas reais para API
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+const mockedAxios = axios as jest.Mocked<typeof axios>; // tem o mesmo tipo que o módulo axios, mas com todas as suas funções substituídas por mock functions. Isso é necessário para que o TypeScript possa verificar se você está chamando as funções corretas e com os argumentos corretos
+
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient();
+};
 
 describe("AxiosHttpClient", () => {
   test("Should call axios with correct URL", async () => {
